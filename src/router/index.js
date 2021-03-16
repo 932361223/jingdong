64450c1e771c3da1435bc485b496e4b41957ce5b
wzt932361223 <>
2021-03-17 00:04:34 +0800

    staticShop

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,26 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/home/Home.vue'
-import Register from '../views/register/Register'
-import Login from '../views/login/Login.vue'
-
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ '../views/home/Home')
+  }, {
+    path: '/shop/:id',
+    name: 'Shop',
+    component: () => import(/* webpackChunkName: "shop" */ '../views/shop/Shop')
   }, {
     path: '/register',
     name: 'Register',
-    component: Register,
+    component: () => import(/* webpackChunkName: "register" */ '../views/register/Register'),
     beforeEnter (to, from, next) {
       const { isLogin } = localStorage;
       isLogin ? next({ name: 'Home' }) : next();
     }
   },
-
   {
     path: '/login',
     name: 'Login',
-    component: Login,
+    component: () => import(/* webpackChunkName: "login" */ '../views/login/Login'),
     beforeEnter: (to, from, next) => {
       const { isLogin } = localStorage
       isLogin ? next({ name: 'Home' }) : next();
--- src/views/home/Nearby.vue
+++ src/views/home/Nearby.vue
@@ -1,27 +1,18 @@
 <template>
   <div class="nearby">
     <h3 class="nearby__title">附近店铺</h3>
-    <div class="nearby__item"
-         v-for="item in nearbyList"
-         :key="item._id">
-      <img :src="item.imgUrl"
-           class="nearby__item__img">
-      <div class="nearby__content">
-        <div class="nearby__content__title">{{item.name}}</div>
-        <div class="nearby__content__tags">
-          <span class="nearby__content__tag">月售: {{item.sales}}</span>
-          <span class="nearby__content__tag">起送: {{item.expressLimit}}</span>
-          <span class="nearby__content__tag">基础运费: {{item.expressPrice}}</span>
-        </div>
-        <p class="nearby__content__highlight">{{item.slogan}}</p>
-      </div>
-    </div>
+    <router-link v-for="item in nearbyList"
+                 :key="item._id"
+                 :to="`/shop/${item._id}`">
+      <ShopInfo :item="item" />
+    </router-link>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 import { get } from '../../utils/request'
+import ShopInfo from '../../components/ShopInfo'
 
 const useNearbyListEffect = () => {
   const nearbyList = ref([]);
@@ -36,6 +27,7 @@ const useNearbyListEffect = () => {
 
 export default {
   name: 'Nearby',
+  components: { ShopInfo },
   setup () {
     const { nearbyList, getNearbyList } = useNearbyListEffect();
     getNearbyList();
@@ -44,7 +36,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../../style/viriables.scss";
 .nearby {
   &__title {
@@ -53,39 +45,8 @@ export default {
     font-weight: normal;
     color: $content-fontcolor;
   }
-  &__item {
-    display: flex;
-    padding-top: 0.12rem;
-    &__img {
-      margin-right: 0.16rem;
-      width: 0.56rem;
-      height: 0.56rem;
-    }
-  }
-  &__content {
-    flex: 1;
-    padding-bottom: 0.12rem;
-    border-bottom: 1px solid $content-bgColor;
-    &__title {
-      line-height: 0.22rem;
-      font-size: 0.16rem;
-      color: $content-fontcolor;
-    }
-    &__tags {
-      margin-top: 0.08rem;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      color: $content-fontcolor;
-    }
-    &__tag {
-      margin-right: 0.16rem;
-    }
-    &__highlight {
-      margin: 0.08rem 0 0 0;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      color: #e93b3b;
-    }
+  a {
+    text-decoration: none;
   }
 }
 </style>
